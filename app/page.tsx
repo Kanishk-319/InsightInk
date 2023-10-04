88e9b873c5ff9a55fb32df361d71a3d2c12b2e25
Kanishk <kanishkpvt01@gmail.com>
2023-10-04 22:11:53 +0530

    Fully Responsive Navbar Finished

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,33 @@
 'use client'
 import { FC } from "react"
+import Link from "next/link";
+import { motion } from "framer-motion";
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import EditRoundedIcon from '@mui/icons-material/EditRounded';
+import ContactSupportRoundedIcon from '@mui/icons-material/ContactSupportRounded';
 
-const Home:FC = () => {
- 
+const Home: FC = () => {
+
   return (
-    <></>
+    <>
+      <div className="w-full flex-col">
+      </div>
+      <nav className="navbar2">
+        <Link href='/'>
+          <HomeRoundedIcon />
+        </Link>
+
+        <Link href='/'>
+          <EditRoundedIcon />
+        </Link>
+
+        <Link href='/'>
+          <ContactSupportRoundedIcon />
+        </Link>
+
+      </nav>
+    </>
+
   )
 }
 
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,9 +1,14 @@
-import { FC } from "react"
+'use client'
+import { FC, useState } from "react" 
+import {motion as m} from 'framer-motion'
 import SearchIcon from '@mui/icons-material/Search';
+import CloseIcon from '@mui/icons-material/Close';
 import Link from "next/link";
 
 const Nav:FC = () => {
+  const [showSearch,setShowSearch] = useState<boolean>(false)
   return (
+    <>
     <nav className="navbar">
             <h3 className="title">Insight<span id="ink">Ink</span></h3>
             <div className="navSection">
@@ -19,14 +24,19 @@ const Nav:FC = () => {
                 </Link>
                 </div>
                 <div className="GetStartedAndSearch">
-                <SearchIcon className="icon"/>
+                <SearchIcon className={`icon ${showSearch?'hidden':'flex'}`} onClick={()=>setShowSearch((prev)=>!prev)}/>
                 <button className="themeButton" id="getStartedbtn">Get Started</button>
                 <button className="themeButton">Login</button>
                 <button className="themeButton" id='signUpbtn'>Sign Up</button>
             </div>
             </div>
-            
     </nav>
+    <div  className={`searchAndCross ${showSearch?'flex':'hidden'}`}>
+    <input type="text" placeholder="..." className={`searchInput ${showSearch?'flex':'hidden'}`}/>
+    <CloseIcon className={`icon ${showSearch?'flex':'hidden'}`} id='cross' onClick={()=>setShowSearch((prev)=>!prev)}/>
+    </div>
+    
+    </>
   )
 }
 
